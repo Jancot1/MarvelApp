29954b87f77ddcfbbbe9c3f92ae322431e39184e
Jancot1 <javier_seco@hotmail.com>
2023-01-13 16:43:46 -0300

    disable redux devtools at env production

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { authSlice } from './auth/AuthSlice';
+import { authSlice } from './auth/authSlice';
 import { albumSlice } from './slices/albumSlice';
 import { characterSlice } from './slices/characterSlice';
 import { comicSlice } from './slices/comicSlice';
 import { eventSlice } from './slices/eventSlice';
 import { searchSlice } from './slices/searchSlice';
 import { uiSlice } from './ui/uiSlice';
+
+const { REACT_APP_ENVIRONMENT } = process.env;
 
 const combinedReducer = combineReducers({
 	auth: authSlice.reducer,
@@ -26,13 +28,5 @@ const rootReducer = ( state, action) => {
 
 export const store = configureStore({
 	reducer: rootReducer,
-	// {
-	//   auth: authSlice.reducer,
-	//   comics: comicSlice.reducer,
-	//   characters: characterSlice.reducer,
-	//   events: eventSlice.reducer,
-	//   search: searchSlice.reducer,
-	//   ui: uiSlice.reducer,
-	//   albums: albumSlice.reducer
-	// },
+	devTools: REACT_APP_ENVIRONMENT !== 'prod',
 });
